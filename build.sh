dfbe83d0a2cd16f9a48e326a0b430d2ae5f1d044
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-01-20 23:07:05 +0100

    update: build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 bash ./thesis/bin/acronyms.sh
 
@@ -6,4 +6,4 @@ cd thesis
 arara DP_Dunaevskiy_Sergey_2022.tex
 cd ..
 
-pdftk thesis/DP_Dunaevskiy_Sergey_2022.pdf cat 17-29 output thesis/DP_Dunaevskiy_Sergey_2022-2.pdf
+pdftk thesis/DP_Dunaevskiy_Sergey_2022.pdf cat 27-39 output thesis/DP_Dunaevskiy_Sergey_2022-2.pdf

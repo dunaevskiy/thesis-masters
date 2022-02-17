c353f0c750eee7ef3be2c7e47f8d810ebca45119
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-02-17 20:35:35 +0100

    add: arch compare

--- _watch-simple.sh
+++ _watch-simple.sh
@@ -4,4 +4,4 @@ cd thesis
 xelatex -shell-escape ./DP_Dunaevskiy_Sergey_2022.tex
 cd ..
 
-pdftk thesis/DP_Dunaevskiy_Sergey_2022.pdf cat 27-39 output thesis/DP_Dunaevskiy_Sergey_2022-2.pdf
+#pdftk thesis/DP_Dunaevskiy_Sergey_2022.pdf cat 27-39 output thesis/DP_Dunaevskiy_Sergey_2022-2.pdf
